ab5712482a4044252b4b44fce69ddf55af245553
Carlos Lisandra <carlos.lisandra@logitravel.com>
2018-05-23 11:27:14 +0200

    added support to netstandard2.0 and unit test webhost build

---
+++ Toolfactory.Prometheus.Extensions.NetCore.Test/TestingStartup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Toolfactory.Prometheus.Extensions.NetCore.Test
+{
+    public class TestingStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UsePrometheus();
+        }
+    }
+}
